cef87929d40420f19d7a6d48b5710f9bd1141d24
Ana.Sousa <anasousa314@outlook.com>
2021-02-15 19:10:09 -0300

    Adiciona data

---
+++ data/most.js
@@ -0,0 +1,33 @@
+let most = [
+  {
+    'caminho_imagem':'/images/most/uma_terra_prometida.webp',
+    'titulo':'Uma Terra Prometida',
+    'tema': 'Biografia',
+    'descricao':'Um relato íntimo e fascinante da história em formação — feito pelo líder que nos inspirou a acreditar no poder da democracia. No comovente e aguardado primeiro volume de suas memórias presidenciais, Barack Obama narra, nas próprias palavras, a história de sua odisseia improvável, desde quando era um jovem em busca de sua identidade até se tornar líder da maior democracia do mundo. ',
+    'preco': 59.99
+  },
+  {
+    'caminho_imagem':'/images/most/o_som_do_nosso_coracao.webp',
+    'titulo':'O Som do Nosso Coração',
+    'tema': 'Romance',
+    'descricao':'“Nossa vida — não importa o que aconteça entre o início e o fim — começa e termina com uma batida do coração: nosso próprio ritmo, nossa própria música.”   A vida da heroína de O som do nosso coração, Melody King, não é nada fácil. Afinal, desde que sofreu um acidente e bateu a cabeça, ela adquiriu um estranho distúrbio: ela canta quando está ansiosa. E não só canta — alto, bem alto e às vezes errado —, como dança, faz performances e emenda uma música na outra, com letras sempre relacionadas à situação em questão. Como se isso já não fosse ruim o bastante, Melody está constantemente ansiosa. Seu marido, Dev, desapareceu há onze anos sem deixar rastros, e ela tem dois filhos adolescentes problemáticos: Flynn — que vive se envolvendo em brigas na escola por causa do bullying que sofre — e a caçula Rose — a supergênia e aluna exemplar que não consegue superar o desaparecimento do pai. Apesar da vergonha constante que sentem da mãe — certa vez, ela cantou uma música do Arctic Monkeys na frente dos colegas de turma de Flynn! —, eles são uma família unida e relativamente feliz, até Rose encontrar um relatório no site de pessoas desaparecidas de alguém cuja descrição bate com a do pai. Será que Dev está vivo, afinal? Ou tudo não passou de uma grande coincidência? Melody pode estar muito perto de encontrar as respostas que têm procurado nos últimos onze anos. Mas ainda que a verdade seja libertadora, também pode destruir tudo o que ela construiu. Será que Melody está pronta para isso? O som do nosso coração é uma história emocionante que cativa o leitor desde a primeira página e que agradará fãs de Jojo Moyes, John Green Nicholas Sparks, Brittainy C. Cherry e Carina Rissi.   “Inspirador, tocante e absurdamente lindo. Ainda não consegui superar esse livro.” – Carina Rissi   “Romântico, bem-humorado e emocionante, o livro de Emma Cooper ficará em seu coração por muito tempo.”  Josie Silver, autora de Um dia em dezembro   “Original e inteligente, este livro vai partir seu coração, depois juntar todos os pedaços de volta.” — Katie Fforde, autora de Uma pitada de amor',
+    'preco': 38.17
+  },
+  {
+    'caminho_imagem':'/images/most/extraordinario.webp',
+    'titulo':'Extraordinario',
+    'tema': 'Infantil',
+    'descricao':'August Pullman, o Auggie, nasceu com uma síndrome genética cuja sequela é uma severa deformidade facial, que lhe impôs diversas cirurgias e complicações médicas. Por isso ele nunca frequentou uma escola de verdade... até agora. Todo mundo sabe que é difícil ser um aluno novo, mais ainda quando se tem um rosto tão diferente. Prestes a começar o quinto ano em um colégio particular de Nova York, Auggie tem uma missão nada fácil pela frente: convencer os colegas de que, apesar da aparência incomum, ele é um menino igual a todos os outros. Narrado da perspectiva de Auggie e também de seus familiares e amigos, com momentos comoventes e outros descontraídos, Extraordinário consegue captar o impacto que um menino pode causar na vida e no comportamento de todos, família, amigos e comunidade - um impacto forte, comovente e, sem dúvida nenhuma, extraordinariamente positivo, que vai tocar todo tipo de leitor.',
+    'preco': 29.89
+  },
+  {
+    'caminho_imagem':'/images/most/alice_no_pais_da_mentira.webp',
+    'titulo':'Uma Terra Prometida',
+    'tema': 'Infantil',
+    'descricao':'Depois de brigar com seu melhor amigo, Alice viaja ao maluquíssimo País da Mentira, descobrindo que as mentiras não são todas iguais. Há algumas até engraçadinhas, como a Mentira Caridosa, mas outras horrorosas, como a Calúnia e a Mentira Cabeluda. Fugindo desta, Alice acaba chegando ao País da Verdade e ali descobre que falar a verdade nem sempre é bom: há algumas, como a Verdade Absoluta, que podem ser tão feias quanto a Mentira Cabeluda! Depois de muito humor, confusões, perigos e descobertas, a menina volta pra casa tendo aprendido a importância do perdão...',
+    'preco': 49.99
+  }
+
+]
+
+module.exports = most
