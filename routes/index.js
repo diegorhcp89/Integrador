ca5dbe81b4443d9e98fc05271c896ed08e983ac7
Diego <diegoferreiradoamaral@gmail.com>
2021-02-15 23:57:14 -0300

    Concluido a pagina de sessões

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ router.get('/products',function(req,res,next){
   res.render('products')
 })
 
+router.get('/section',function(req,res,next){
+  res.render('section')
+})
+
 /* coloquei em users.js */
 
 module.exports = router;
