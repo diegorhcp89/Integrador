6381d4debb5d58c7391a85586d3b3089e753ea17
Thais-Abe <abe.c.thais@gmail.com>
2021-02-15 23:01:43 -0300

    adicionando a página de pagamentos

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,19 @@ router.get('/', function(req, res, next) {
 router.get('/products',function(req,res,next){
   res.render('products')
 })
-
 /* coloquei em users.js */
+
+/* GET cadastro page */
+
+router.get('/cadastro', function(req, res, next){
+  res.render('cadastro')
+})
+
+/* GET pagamentos page */
+
+router.get('/pagamentos', function(req, res, next){
+  res.render('pagamentos')
+})
+
 
 module.exports = router;
