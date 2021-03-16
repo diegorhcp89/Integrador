38ca2e8186e138b03e8c4b6dc500ffcc7135e4fc
Ana.Sousa <anasousa314@outlook.com>
2021-03-16 16:12:11 -0300

    Alteracao no arquivo database.js da pasta config.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
  const config = {
-    "username": "root",
-    "password": '',
-    "database": "integrador",
-    "host": "localhost",
-    "dialect": "mysql"
+    username: "root",
+    password: '',
+    database: "integrador",
+    host: "localhost",
+    dialect: "mysql"
   }
+
+  module.exports = config;
