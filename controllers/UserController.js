14b79b838a7e90bb1447165810e4859e90731b2d
Ana.Sousa <anasousa314@outlook.com>
2021-03-18 15:10:36 -0300

    Alteracao do metodo save para criptografar senha e salvar user no banco de dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const router = express.Router();
 const valida = require('gerador-validador-cpf');
-// const teste = valida.validate() 
 
+const {User} = require('../models');
+const bcrypt = require('bcrypt');
 
 
 module.exports = {
@@ -14,24 +13,44 @@ module.exports = {
     res.render('cadastro')
   },
 
-  save(req,res,next){
-    let senha = req.body.senha;
-    let confSenha = req.body.confSenha;
+  async save(req,res,next){
 
-    if(senha != confSenha){
+    if(req.body.password != req.body.confPassword){
       return res.render('cadastro',{validaSenha: false})
+    }else{
+      req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+
+      req.body.confPassword = bcrypt.hashSync(req.body.confPassword, bcrypt.genSaltSync(10));
     }
 
     let validaCpf = valida.validate(req.body.cpf);
-    console.log(validaCpf)
+    //console.log(validaCpf)
 
 
     /* Verificar como melhorar isso */
     if (validaCpf==false) {
       res.render('cadastro',{valiCpf: false});
     }
+    let infoEmail = ''
 
-    console.log(req.body)
+    if(req.body.infoEmail != undefined){
+      infoEmail = "true"
+    }else{
+      infoEmail = "false"
+    }
 
+    let user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      cpf: req.body.cpf,
+      gender: req.body.gender,
+      infoEmail
+    };
+
+    await User.create(user)
+
+    console.log(user)
+   res.render('cadastro',{added:true});
   }
 }
