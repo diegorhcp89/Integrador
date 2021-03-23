5087ba1f22030bcadeaf6626a4220f7886af4aef
Ana.Sousa <anasousa314@outlook.com>
2021-03-23 16:21:19 -0300

    Cria a migration sales para a tabela de promocoes

---
+++ database/migrations/20210323190003-create-sales.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable('sales', { 
+      id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      percent:{
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      book_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+          model: 'books',
+          key: 'id'
+        }
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+
+     });
+    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+     await queryInterface.dropTable('sales');
+     
+  }
+};
