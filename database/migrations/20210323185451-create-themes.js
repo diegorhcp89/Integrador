fe03b4642bcfe8ffba24cac257b653f7dd288348
Ana.Sousa <anasousa314@outlook.com>
2021-03-23 16:19:40 -0300

    Cria a migration themes para a tabela de temas

---
+++ database/migrations/20210323185451-create-themes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('themes',{
+      id:{
+        allowNull: false,
+        autoIncrement:true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      theme:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('themes');
+  }
+};
