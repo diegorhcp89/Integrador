a7e6db3cb2f90fcf2b5c816266a94fa0e5a4335c
Ana.Sousa <anasousa314@outlook.com>
2021-04-07 16:01:04 -0300

    Cria o model Book.js com associacao

---
+++ models/Book.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize,DataTypes)=>{
+  const Book = sequelize.define('Book',{
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title:{  
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    imgPath:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description:{ 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    themeId:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },{
+    tableName: 'books'
+  });
+
+  // Book.associate = function(models){
+  //   Book.hasMany(models.Theme,{as: 'theme'})
+  // }
+  Book.associate = function(models){
+      Book.belongsToMany(models.User,{
+        foreignKey: 'userId', through: 'favorites',as: 'users'
+      });
+      Book.belongsTo(models.Theme,{foreignKey:'themeId', as: 'theme'})
+  }
+  return Book;
+};
