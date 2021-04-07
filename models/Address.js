4a1aa51ecf02533322fc8a27b8944640f6a2ac35
Ana.Sousa <anasousa314@outlook.com>
2021-04-07 15:59:57 -0300

    Cria o model Address.js

---
+++ models/Address.js
@@ -0,0 +1,50 @@
+module.exports = (sequelize,DataTypes)=>{
+  const Address = sequelize.define('Address',{
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    state:{  
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    city:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    street:{ 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    district:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    number:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    cep:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    complement:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    reference:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    userId:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },{
+    tableName: 'addresses'
+  });
+
+  return Address;
+};
