6d225db1b806e6ddecbbc1e7eaeb4cf22acf79cc
Ana.Sousa <anasousa314@outlook.com>
2021-04-09 18:14:02 -0300

    Adiciona metodo create ao FavoritesController.js

--- controllers/FavoriteController.js
+++ controllers/FavoriteController.js
@@ -1,29 +1,47 @@
 const {User} = require('../models');
-const {Book }= require('../models')
+const {Book }= require('../models');
+const {Favorite} = require('../models');
 
 module.exports = {
- async index(req,res){
+ async index(req,res,next){
     const userId = req.params.id;
 
     console.log(userId)
 
-    const user = await User.findByPk(userId,{
-      include:{
-        association: 'books',
-        attributes: ['title'],
-        through:{
-          attributes: []
-        }
-      }
-    })
+    // const user = await User.findByPk(userId,{
+    //   include:{
+    //     association: 'books',
+    //     attributes: ['title'],
+    //     through:{
+    //       attributes: []
+    //     }
+    //   }
+    // })
 
+    const user = await User.findByPk(userId)
      console.log(user)
+
+    
+    
   // return res.json(user.books)
 
     //res.render('favorites')
   },
 
-  // async create(req,res){
-  //   const {}
-  // }
+  async create(req,res,next){
+    let bookId = req.params.id
+    bookId = parseInt(bookId)
+    console.log(bookId)
+
+    const userId = req.session.user
+    console.log(userId.id);
+
+    let favorite = {userId: userId.id,bookId};
+
+    if(typeof userId != undefined){
+      await Favorite.create(favorite);
+    }
+
+
+  }
 }
