5ef8df71adf7fde1aeeeeb07f4cfe85b901ce0c4
Ana.Sousa <anasousa314@outlook.com>
2021-04-09 18:12:29 -0300

    Adiciona a rota create em routes/favorites.js

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,4 +4,6 @@ const FavoriteController = require('../controllers/FavoriteController')
 
 router.get('/index/:id', FavoriteController.index);
 
+router.get('/create/:id', FavoriteController.create)
+
 module.exports = router
