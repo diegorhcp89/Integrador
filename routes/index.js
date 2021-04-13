633ad2ce1084cd219b9a70e93a62e93b423db07c
Ana.Sousa <anasousa314@outlook.com>
2021-04-13 11:27:00 -0300

    Inclui os temas dinamicamente na rota /

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,34 @@ var router = express.Router();
 const promotions = require('../data/promotions')
 const most = require('../data/most')
 
+const {Theme} = require('../models');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',{promotions: promotions, most:most, user: req.session.user});
+router.get('/', async function(req, res, next) {
+  let themes = await Theme.findAll();
+
+  console.log(themes);
+
+  res.render('index',{promotions: promotions, most:most, user: req.session.user, themes});
 });
 
 /* GET products page. */
 router.get('/products',function(req,res,next){
   res.render('products',{user: req.session.user})
-})
-
-router.get('/section',function(req,res,next){
-  res.render('section',{user: req.session.user})
-})
-
-// router.get('/favorites',function(req,res,next){
-  // res.render('favorites')
-// })
+});
 
 /* coloquei em users.js */
 
 router.get('/payment',function(req,res,next){
   res.render('payment',{user: req.session.user})
-})
+});
 
 router.get('/cart',function(req,res,next){
   res.render('cart',{user: req.session.user})
-})
+});
 
 router.get('/concluded',function(req,res,next){
   res.render('concluded',{user: req.session.user})
-})
+});
 
 module.exports = router;
