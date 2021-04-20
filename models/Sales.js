06fedb29beaa012a3a36e54ac2b6ba5f25a2deb2
Ana.Sousa <anasousa314@outlook.com>
2021-04-20 08:50:30 -0300

    Altera a model Sales.js

--- models/Sales.js
+++ models/Sales.js
@@ -10,7 +10,7 @@ module.exports = (sequelize,DataTypes)=>{
       type: DataTypes.DECIMAL,
       allowNull: false
     }, 
-    book_id:{
+    bookId:{
       type: DataTypes.INTEGER,
       allowNull: false
     }
