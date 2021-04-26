6bdd86c3aa9e3ae1fbc405b2f60c49c18de30066
Ana.Sousa <anasousa314@outlook.com>
2021-04-26 15:48:40 -0300

    Adiciona metodo search em SectionController

--- controllers/SectionController.js
+++ controllers/SectionController.js
@@ -1,12 +1,24 @@
-const {Book} = require('../models');
+const {Book, Sequelize} = require('../models');
 const {Theme} = require('../models')
 
 module.exports = {
   async produtos(req,res,next){
     let books = await Book.findAll();
-
     console.log(books)
     res.render('section',{user: req.session.user,themesAll: true, books});
+  },
+
+  async search(req,res,next){
+    let {word} = req.query;
+
+    let books = await Book.findAll({
+      where:{
+        title:{[Sequelize.Op.like]: `%${word}%`},
+      },
+      order: ['id']
+    });
+    console.log(books)
+    res.render('section',{user: req.session.user,search: true, books, word});
   },
 
   async theme(req,res,next){
